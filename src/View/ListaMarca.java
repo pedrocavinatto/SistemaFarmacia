e0588ee6f2d7d6c9e8e5416be1c95c3060a91ab0
Carlos Gabriel <carlosgabrielmennabarretocor@gmail.com>
2023-06-21 10:44:05 -0300

    Update ListaMarca.java
    adicionada a mesma funcionalidade, agora para ListaMarca

--- src/View/ListaMarca.java
+++ src/View/ListaMarca.java
@@ -9,6 +9,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -46,16 +48,29 @@ public class ListaMarca extends JFrame {
 	 */
 	public ListaMarca() {
 		Map<Integer, Integer> rowId_marcaId = new HashMap<>();
-		
+
 		setTitle("Marcas");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 634, 524);
+
+		setBounds(100, 100, 649, 508);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		// Create an instance of the Menu, but keep it invisible for now
+		Menu menu = new Menu();
+		menu.setVisible(false);
+
+		// Add a window listener to the ListaRemedio window
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// When the ListaRemedio window is closed, make the Menu window visible
+				menu.setVisible(true);
+			}
+		});
+
 		JButton btnCadastraMarca = new JButton("Cadastrar marca");
 		btnCadastraMarca.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -67,26 +82,26 @@ public class ListaMarca extends JFrame {
 		btnCadastraMarca.setBounds(10, 425, 187, 52);
 		btnCadastraMarca.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		contentPane.add(btnCadastraMarca);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 10, 600, 405);
 		contentPane.add(scrollPane);
-		
+
 		tbMarcas = new JTable();
 		scrollPane.setViewportView(tbMarcas);
 		tbMarcas.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Nome", "CNPJ", "Telefone"
-			}
-		) {
+				new Object[][] {
+				},
+				new String[] {
+						"Nome", "CNPJ", "Telefone"
+				}) {
 			Class[] columnTypes = new Class[] {
-				String.class, String.class, String.class
+					String.class, String.class, String.class
 			};
 			boolean[] columnEditables = new boolean[] {
-				false, false, false
+					false, false, false
 			};
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
@@ -95,7 +110,7 @@ public class ListaMarca extends JFrame {
 		tbMarcas.getColumnModel().getColumn(1).setPreferredWidth(100);
 		tbMarcas.getColumnModel().getColumn(2).setPreferredWidth(100);
 		atualizaLista(tbMarcas, rowId_marcaId);
-		
+
 		JButton btnEditaMarca = new JButton("Editar marca");
 		btnEditaMarca.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -108,20 +123,21 @@ public class ListaMarca extends JFrame {
 		btnEditaMarca.setBounds(226, 425, 187, 52);
 		contentPane.add(btnEditaMarca);
 	}
-	
+
 	private void atualizaLista(JTable tbMarcas, Map<Integer, Integer> rowId_marcaId) {
 		ControleMarca controle_marca = new ControleMarca();
 		List<Marca> marcas = controle_marca.listaMarcas();
-		
-		int i = -1; //contador do ID linha
+
+		int i = -1; // contador do ID linha
 		for (Marca marca : marcas) {
 			i++;
 			DefaultTableModel model = (DefaultTableModel) tbMarcas.getModel();
-			Object[] newRow = {marca.getNome(), marca.getCnpj(), marca.getTelefone(), marca.getId()};
+			Object[] newRow = { marca.getNome(), marca.getCnpj(), marca.getTelefone(), marca.getId() };
 			model.addRow(newRow);
 			rowId_marcaId.put(i, marca.getId());
 		}
 	}
+
 	private int retornaIdMarcaSelecionada(JTable tbMarcas, Map<Integer, Integer> rowId_marcaId) {
 		int rowIndex = tbMarcas.getSelectedRow();
 		return rowId_marcaId.get(rowIndex);
