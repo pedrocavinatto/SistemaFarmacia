fdea740023a21701ed90152479201ce864ed771f
Carlos Gabriel <carlosgabrielmennabarretocor@gmail.com>
2023-06-21 10:41:53 -0300

    Update ListaVenda.java
    mesma funcionalidade, agora implementada na listaVenda

--- src/View/ListaVenda.java
+++ src/View/ListaVenda.java
@@ -8,6 +8,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,16 +49,26 @@ public class ListaVenda extends JFrame {
 	 */
 	public ListaVenda() {
 		Map<Integer, Integer> rowId_vendaId = new HashMap<>();
-		
+
 		setTitle("Vendas");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 647, 506);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		setBounds(100, 100, 649, 508);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		Menu menu = new Menu();
+		menu.setVisible(false);
+
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				menu.setVisible(true);
+			}
+		});
+
 		JButton btnCadastraVenda = new JButton("Cadastrar venda");
 		btnCadastraVenda.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -68,28 +80,30 @@ public class ListaVenda extends JFrame {
 		btnCadastraVenda.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnCadastraVenda.setBounds(10, 407, 187, 52);
 		contentPane.add(btnCadastraVenda);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 10, 613, 387);
 		contentPane.add(scrollPane);
-		
+
 		tbVendas = new JTable();
 		tbVendas.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"M\u00E9todo de pagamento", "Data da venda", "Valor total"
-			}
-		) {
+				new Object[][] {
+				},
+				new String[] {
+						"M\u00E9todo de pagamento", "Data da venda", "Valor total"
+				}) {
 			Class[] columnTypes = new Class[] {
-				String.class, String.class, Double.class
+					String.class, String.class, Double.class
 			};
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
+
 			boolean[] columnEditables = new boolean[] {
-				false, false, false
+					false, false, false
 			};
+
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
@@ -98,15 +112,17 @@ public class ListaVenda extends JFrame {
 		scrollPane.setViewportView(tbVendas);
 		atualizaLista(tbVendas, rowId_vendaId);
 	}
+
 	private void atualizaLista(JTable tbVendas, Map<Integer, Integer> rowId_vendaId) {
 		ControleVenda controle_venda = new ControleVenda();
 		List<Venda> vendas = controle_venda.listaVendas();
-		
-		int i = -1; //contador do ID linha
+
+		int i = -1; // contador do ID linha
 		for (Venda venda : vendas) {
 			i++;
 			DefaultTableModel model = (DefaultTableModel) tbVendas.getModel();
-			Object[] newRow = {venda.getMetodoPagamento().toString(), venda.getDataVenda().toString(), venda.getValorTotal()};
+			Object[] newRow = { venda.getMetodoPagamento().toString(), venda.getDataVenda().toString(),
+					venda.getValorTotal() };
 			model.addRow(newRow);
 			rowId_vendaId.put(i, venda.getId());
 		}
